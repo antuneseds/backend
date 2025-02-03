e76ba8a20b5bb7344d349b5d5d767c3843da90fc
antuneseds <esterdossantosantunes04@gmail.com>
2025-02-03 11:14:07 -0300

    Aula 1

--- alunos/models.py
+++ alunos/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+#Create your models here.
+class Estado (models.Model):
+    nome = models.CharField(max_length=50)
+    sigla = models.CharField(max_length=2)
+
+    def __str__(self):
+        return self.nome
+    
