c076b42e2ee098931eb2c0102500d88abe7a9a9f
antuneseds <esterdossantosantunes04@gmail.com>
2025-02-10 11:36:29 -0300

    Aula 2

---
+++ alunos/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework.serializers import ModelSerializer
+from alunos.models import Estado
+
+
+class EstadoSerializer(ModelSerializer):
+    class Meta:
+        model = Estado
+        fields = '__all__'
